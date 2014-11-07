85357f0677c80442a576cd0692599130d179e4e8
Tunghsiao Liu <t@sparanoid.com>
2014-11-07 09:35:05 +0800

    chore: use secure URL by default

--- jquery.gravatar.js
+++ jquery.gravatar.js
@@ -23,7 +23,7 @@
       // url to define a default image (can also be one of: identicon, monsterid, wavatar)
       image: '',
       // secure
-      secure: false,
+      secure: true,
       // support css on img element
       classes: ''
     }, overrides);
